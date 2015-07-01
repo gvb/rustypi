2b2dbffee45742deb0698adecb20436645a7e7ca
Gerald Van Baren <gvb@unssw.com>
2015-06-30 20:41:48 -0400

    Improve the GPIO configuration routines
    Had to save and restore the counter register in delay() - the optimizer
    assumed it was not clobbered, but we clobbered it in our countdown
    loop. It would be better to tell the optimizer that it is clobbered.
    
    Signed-off-by: Gerald Van Baren <gvb@unssw.com>

--- rpi/src/lib.rs
+++ rpi/src/lib.rs
@@ -26,9 +26,9 @@ pub enum MemMap {
 
 // Loop <delay> times, using "volatile" to prevent the optimizer from
 // optimizing it to nothing.
-fn delay(count: i32) -> () {
+fn delay(count: usize) -> () {
     unsafe {
-        asm!("1: subs $0, $0, #1; bne 1b\n"
+        asm!("push {$0}; 1: subs $0, $0, #1; bne 1b; pop {$0}\n"
              :
              : "r"(count)
              : "cc"
@@ -58,12 +58,82 @@ mod mmio {
  * GPIO - General Purpose Input / Output
  ***************************************************************************/
 
+#[allow(dead_code)]
 enum GpioMemMap {
-    // Controls actuation of pull up/down to ALL GPIO pins.
+    // Function selections
+    GPFSEL0 = 0x00,
+    GPFSEL1 = 0x04,
+    GPFSEL2 = 0x08,
+    GPFSEL3 = 0x0C,
+    GPFSEL4 = 0x10,
+    GPFSEL5 = 0x14,
+
+    // Pin control (set/clear/level)
+    GPSET0 = 0x1C,
+    GPSET1 = 0x20,
+
+    GPCLR0 = 0x28,
+    GPCLR1 = 0x2C,
+
+    GPLEV0 = 0x34,
+    GPLEV1 = 0x38,
+
+    // Event detect status
+    GPEDS0 = 0x40,
+    GPEDS1 = 0x44,
+
+    // Rising edge detect enables
+    GPREN0 = 0x4C,
+    GPREN1 = 0x50,
+
+    // Falling edge detect enables
+    GPFEN0 = 0x58,
+    GPFEN1 = 0x5C,
+
+    // High detect enables
+    GPHEN0 = 0x64,
+    GPHEN1 = 0x68,
+
+    // Low detect enables
+    GPLEN0 = 0x70,
+    GPLEN1 = 0x74,
+
+    // Async rising edge detect enables
+    GPAREN0 = 0x7C,
+    GPAREN1 = 0x80,
+
+    // Async falling edge detect enables
+    GPAFEN0 = 0x88,
+    GPAFEN1 = 0x8C,
+
+    // Pin pull-up/down enables
     GPPUD = 0x94,
  
-    // Controls actuation of pull up/down for specific GPIO pin.
+    // Pin pull-up/down enable clocks
     GPPUDCLK0 = 0x98,
+    GPPUDCLK1 = 0x9C,
+}
+
+#[allow(dead_code)]
+pub enum GpioFunctionSelect {
+    // Function selections for the pins.
+    // Note the alt function selections are numbered a little oddly.
+    Input = 0,
+    Output = 1,
+    Alt0 = 4,
+    Alt1 = 5,
+    Alt2 = 6,
+    Alt3 = 7,
+    Alt4 = 3,
+    Alt5 = 2,
+}
+ 
+#[allow(dead_code)]
+pub enum GpioPullUpDown {
+    // GPIO pin pullup/down disabled, pull up, pull down.
+    Off = 0,
+    PullDown = 1,
+    PullUp = 2,
 }
  
 pub struct Gpio {
@@ -74,20 +144,76 @@ impl Gpio {
 
     // TODO: Verify operation and comment
     pub fn init(&self) {
-        mmio::write(self.base_addr + GpioMemMap::GPPUD as usize, 0x00000000);
+    }
+
+//  pub fn config_uart0(&self) {
+//      mmio::write(self.base_addr + GpioMemMap::GPPUD as usize, 0x00000000);
+//      delay(150);
+//
+//      mmio::write(self.base_addr + GpioMemMap::GPPUDCLK0 as usize,
+//                 (1 << 14) | (1 << 15));
+//      delay(150);
+//
+//      // Write a zero to GPPUDCLK0 to make it take effect.
+//      mmio::write(self.base_addr + GpioMemMap::GPPUDCLK0 as usize, 0x00000000);
+//  }
+
+    pub fn config_pull_up_down(&self, pin: usize, config: GpioPullUpDown) {
+        // Ref: BCM2834 ARM Peripherals manual, page 95
+
+        // Configure the control: off/pulldown/pullup
+        mmio::write(self.base_addr + GpioMemMap::GPPUD as usize,
+                    config as usize);
+        // Required setup time
         delay(150);
- 
-        // Write a zero to GPPUDCLK0 to make it take effect.
-        mmio::write(self.base_addr + GpioMemMap::GPPUDCLK0 as usize, 0x00000000);
+        // Clock the appropriate pin's clock
+        match pin {
+            0 ... 31 =>
+                mmio::write(self.base_addr + GpioMemMap::GPPUDCLK0 as usize,
+                            1 << (pin as usize)),
+            32 ... 53 =>
+                mmio::write(self.base_addr + GpioMemMap::GPPUDCLK1 as usize,
+                            1 << (pin as usize - 32)),
+            _ => {}
+        }
+        // Required setup time
+        delay(150);
+        // Write to the clock register
+        match pin {
+            0 ... 31 =>
+                mmio::write(self.base_addr + GpioMemMap::GPPUDCLK0 as usize, 0),
+            32 ... 53 =>
+                mmio::write(self.base_addr + GpioMemMap::GPPUDCLK1 as usize, 0),
+            _ => {}
+        }
+    }
+
+    pub fn config_function(&self, pin: usize, config: GpioFunctionSelect) {
+        let (adjpin, reg) = match pin {
+            0 ... 9 =>
+                (pin,      self.base_addr + GpioMemMap::GPFSEL0 as usize),
+            10 ... 19 =>
+                (pin - 10, self.base_addr + GpioMemMap::GPFSEL1 as usize),
+            20 ... 29 =>
+                (pin - 20, self.base_addr + GpioMemMap::GPFSEL2 as usize),
+            30 ... 39 =>
+                (pin - 30, self.base_addr + GpioMemMap::GPFSEL3 as usize),
+            40 ... 49 =>
+                (pin - 40, self.base_addr + GpioMemMap::GPFSEL4 as usize),
+            50 ... 53 =>
+                (pin - 50, self.base_addr + GpioMemMap::GPFSEL5 as usize),
+            _ => (0,0)
+        };
+        mmio::write(reg, (mmio::read(reg) & 0b111 << (adjpin * 3)) |
+                         (config as usize) << (adjpin * 3));
     }
 
     pub fn config_uart0(&self) {
-        mmio::write(self.base_addr + GpioMemMap::GPPUDCLK0 as usize,
-                   (1 << 14) | (1 << 15));
-        delay(150);
- 
-        // Write a zero to GPPUDCLK0 to make it take effect.
-        mmio::write(self.base_addr + GpioMemMap::GPPUDCLK0 as usize, 0x00000000);
+        // Disable the pullup/down on the UART pins
+        self.config_pull_up_down(14, GpioPullUpDown::Off);
+        self.config_pull_up_down(15, GpioPullUpDown::Off);
+//      self.config_function(14, GpioFunctionSelect::Alt0);
+//      self.config_function(15, GpioFunctionSelect::Alt0);
     }
 }
 
@@ -132,7 +258,7 @@ impl Uart {
 
     pub fn init(&self) {
         // Clear pending interrupts.
-        mmio::write(self.base_addr + UartMemMap::ICR as usize, 0x7FF);
+        mmio::write(self.base_addr + UartMemMap::ICR as usize, 0x07FF);
  
         // Set integer & fractional part of baud rate.
         // Divider = UART_CLOCK/(16 * Baud)
@@ -144,7 +270,7 @@ impl Uart {
         mmio::write(self.base_addr + UartMemMap::IBRD as usize, 1);
         mmio::write(self.base_addr + UartMemMap::FBRD as usize, 40);
  
-        // Enable FIFO & 8 bit data transmissio (1 stop bit, no parity).
+        // Enable FIFO & 8 bit data transmission (1 stop bit, no parity).
         mmio::write(self.base_addr + UartMemMap::LCRH as usize,
                    (1 << 4) | (1 << 5) | (1 << 6));
  
